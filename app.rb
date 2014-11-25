76b9efdaf722b810d98390b5ff36614eac6b8715
CF <charlotte.feather@yahoo.com>
2014-11-25 23:34:05 +0000

    added form

--- app.rb
+++ app.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
 
-get '/' do
-	"Hello world!!!  I'm here. booo"
-end
+
+ get '/' do
+   erb :form
+ end
+
+ post '/' do
+   @name = params[:user_name]
+   @people  = params[:people].to_i
+   @mood  = params[:mood].to_i
+
+   @places= ['London Eye', 'Globe', 'Shordich']
+
+
+   if (@people+ @mood) >= 10
+     erb :drink
+   elsif
+   	(@people+ @mood) >= 5
+     erb :no_drink
+     else
+   	
+     erb :Shordich
+
+
+
+   end
+ end
